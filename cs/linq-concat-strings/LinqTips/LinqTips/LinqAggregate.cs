e2b04c3ba8d7dace8e3288b6016c6677edd653ba
kanekoh <kaneko.h.ai@gmail.com>
2022-11-15 00:06:43 +0900

    add linq tips

---
+++ cs/linq-concat-strings/LinqTips/LinqTips/LinqAggregate.cs
@@ -0,0 +1,20 @@
+﻿namespace LinqTips;
+
+public class LinqAggregate
+{
+    public static void Execute()
+    {
+        var dictionary = new Dictionary<string, string>
+        {
+            ["key1"] = "value1",
+            ["key2"] = "value2",
+            ["key3"] = "value3",
+        };
+
+        var aggregated = dictionary
+                            .Select(x => $"{x.Key} : {x.Value}")
+                            .Aggregate((current, next) => $"{current} --- {next}");
+
+        Console.WriteLine($"{nameof(LinqAggregate)} result...  {aggregated}");
+    }
+}
