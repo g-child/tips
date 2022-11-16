68362db007fd46729f50d673baad221af4523684
kanekoh <kaneko.h.ai@gmail.com>
2022-11-17 00:19:30 +0900

    .NET 6 ValidateOnStart

---
+++ cs/AspNetCoreTips/AspNetCoreTips.NetCore6Ver/Options/ValidatableOption.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreTips.NetCore6Ver.Options
+{
+    public class ValidatableOption : IValidatableObject
+    {
+        public string? NonNull { get; set; }
+        public int MoreThan1 { get; set; }
+
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (NonNull is null) { yield return new ValidationResult("not null", new[] { nameof(NonNull) }); }
+            if (MoreThan1 < 1) { yield return new ValidationResult("< 1", new[] { nameof(MoreThan1) }); }
+        }
+    }
+}
--- cs/AspNetCoreTips/AspNetCoreTips.NetCore6Ver/Program.cs
+++ cs/AspNetCoreTips/AspNetCoreTips.NetCore6Ver/Program.cs
@@ -1,6 +1,15 @@
+using AspNetCoreTips.NetCore6Ver.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// ValidateOpnStart. ASP.NET 6 より追加された。これを使うとアプリ起動時 (app.Run()) のタイミングに検証を行い、失敗したら落ちるようになる。
+builder.Services.AddOptions<ValidatableOption>()
+    .Bind(builder.Configuration.GetSection(nameof(ValidatableOption)))
+    .ValidateDataAnnotations()
+    .ValidateOnStart();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
