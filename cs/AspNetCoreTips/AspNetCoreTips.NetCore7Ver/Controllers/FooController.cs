80e6f9af6d4a1f89e799c30d0cd910f342a907e4
kanekoh <kaneko.h.ai@gmail.com>
2022-11-17 23:57:45 +0900

    add netcore 7

---
+++ cs/AspNetCoreTips/AspNetCoreTips.NetCore7Ver/Controllers/FooController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreTips.NetCore7Ver.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FooController : ControllerBase
+    {
+        [HttpGet("Sample")]
+        public async Task<IActionResult> SampleAsync()
+        {
+            return await Task.FromResult(Ok());
+        }
+    }
+}
