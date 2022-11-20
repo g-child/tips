d5b42c2cf590f173d763df9840c9b9b7033f41cc
kanekoh <kaneko.h.ai@gmail.com>
2022-11-21 01:50:23 +0900

    add blazingpizza

---
+++ cs/BlazorMsLearn/BlazorMsLearn.BlazingPizzaSite/Model/PizzaSpecial.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorMsLearn.BlazingPizzaSite.Model;
+
+public class PizzaSpecial
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public decimal BasePrice { get; set; }
+    public required string Description { get; set; }
+    public required string ImageUrl { get; set; }
+    public string GetFormattedBasePrice() => BasePrice.ToString("0.00");
+}
