59934c8b2ea8e44b3433c634c0b3724886cd6af3
kanekoh <kaneko.h.ai@gmail.com>
2022-11-23 21:28:20 +0900

    pooh

---
+++ cs/BlazorMsLearn/BlazorMsLearn.BlazingPizzaSite/Model/PizzaTopping.cs
@@ -0,0 +1,7 @@
+namespace BlazorMsLearn.BlazingPizzaSite.Model;
+
+public class PizzaTopping
+{
+    public required string Name { get; set; }
+    public required string Ingredients { get; set; }
+}
