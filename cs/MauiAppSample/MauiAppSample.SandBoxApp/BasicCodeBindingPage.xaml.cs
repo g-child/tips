237c5467dad192b8e4906c8b5f1b2c78640cdf2f
kanekoh <kaneko.h.ai@gmail.com>
2022-12-05 00:39:14 +0900

    add inheritance binding sample

--- cs/MauiAppSample/MauiAppSample.SandBoxApp/BasicCodeBindingPage.xaml.cs
+++ cs/MauiAppSample/MauiAppSample.SandBoxApp/BasicCodeBindingPage.xaml.cs
@@ -5,8 +5,5 @@ public partial class BasicCodeBindingPage : ContentPage
 	public BasicCodeBindingPage()
 	{
 		InitializeComponent();
-
-		label.BindingContext = slider;
-		label.SetBinding(RotationProperty, "Value");
 	}
 }
